3a27f45b6175e55b662433d862a4598cf6eef52a
Vlad Bibire <33767184+webexplained@users.noreply.github.com>
2018-11-09 19:57:55 +0000

    Final touches / Version 1.0 Done

---
+++ dist/precache-manifest.983735732c6c7ce6f0d5636eb864d58d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56d44b5f589003bfda7b",
+    "url": "/static/js/main.56d44b5f.chunk.js"
+  },
+  {
+    "revision": "4d17e399edeb41261d29",
+    "url": "/static/js/1.4d17e399.chunk.js"
+  },
+  {
+    "revision": "56d44b5f589003bfda7b",
+    "url": "/static/css/main.c7d0fc9a.chunk.css"
+  },
+  {
+    "revision": "6c74c38a7be7fd187e2868478fd6c06d",
+    "url": "/index.html"
+  }
+];
